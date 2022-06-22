f39d126ebbb3c5008141c461dd9073e1f613b988
julihchic <101681736+julihchic@users.noreply.github.com>
2022-06-22 15:26:54 -0500

    Update question.sh

--- question.sh
+++ question.sh
@@ -41,20 +41,7 @@
 #
 #  >>> Escriba su codigo a partir de este punto <<<
 #
-#sed 's/\([0-9][0-9]\)\/\([0-9][0-9]\)\/\([0-9][0-9]\)/\3-\2-\1/g' data.csv > result1.csv
-#sed 's/\([0-9][0-9]\)-/20\1-/' result1.csv > result2.csv
-#sed 's/\([0-9]\)\//0\1\//g' result2.csv > result3.csv
-#sed 's/\([0-9][0-9]\)\/\([0-9][0-9]\)\/\([0-9][0-9][0-9][0-9]\)/\3-\2-\1/g' result3.csv > item1.csv
-#sed 's/;;/;\\N;/' item1.csv > r1.csv
-#sed '4 s/;\\N/;\\N;\\N/' r1.csv > r2.csv
-#sed '6 s/;n/;n;\\N/' r2.csv > r3.csv
-#sed '7 s/;\\N;/;\\N;\\N;\\N/' r3.csv > r4.csv
-#sed '9 s/;\\N/;\\N;\\N/' r4.csv > r5.csv
-#sed '5 s/;\\N;\\N;/;\\N;\\N;\\N;\\N/' r5.csv > r6.csv
-#sed '10 s/;\\N;\\N/;\\N;\\N;\\N/' r6.csv > item2.csv
-#sed 's/;/,/g' item2.csv > item3.csv
-#sed 's/,\([0-9][0-9][0-9]\)\,\([0-9]\)/,\1\.\2/' item3.csv > output.csv
-#sed 's/,/./g' data.csv | sed 's/;/,/g' | sed -E 's,([0-9]{2})/([0-9]{2})/([0-9]{2}),20\3-\2-\1,g' | sed -E 's,([0-9]{1})/([0-9]{1})/([0-9]{4}),\3-0\2-0\1,g' |sed 's/,,/\,\\N,/g' | sed 's/[a-z]/\U&/g'|sed 's/,N/,\\N/g'|sed 's/,$/,\\N/'
+
 sed 's/,/./g' data.csv > d1.csv
 sed 's/;/,/g' d1.csv > d2.csv
 sed -E 's,([0-9]{2})/([0-9]{2})/([0-9]{2}),20\3-\2-\1,g' d2.csv > d3.csv
